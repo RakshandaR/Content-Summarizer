040cc2075d261c0f056020c83687226601f88b24
RakshandaR <36320806+RakshandaR@users.noreply.github.com>
2026-01-14 22:29:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from langchain_community.document_loaders import YoutubeLoader,UnstructuredURLLo
 
 
 ## sstreamlit APP
-st.set_page_config(page_title="LangChain: Summarize Text From YT or Website", page_icon="🦜")
-st.title("🦜 LangChain: Summarize Text From YT or Website")
+st.set_page_config(page_title="LangChain: Summarize Text From YT or Website")
+st.title("LangChain: Summarize Text From YT or Website")
 st.subheader('Summarize URL')
 
 
@@ -53,4 +53,5 @@ if st.button("Summarize the Content from YT or Website"):
                 st.success(output_summary)
         except Exception as e:
             st.exception(f"Exception:{e}")
+
                     
